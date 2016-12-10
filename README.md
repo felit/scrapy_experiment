5e1a9dec54d76e36e393da7f7b60e486658c629e
felit <nofelit@126.com>
2016-12-10 13:59:18 +0800

    添加css选择器实验代码

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+https://www.w3.org/TR/selectors/
+https://www.w3.org/TR/xpath/
--- scrapy_experiment/spiders/css_selector.py
+++ scrapy_experiment/spiders/css_selector.py
@@ -8,4 +8,13 @@ class CssSelectorSpider(scrapy.Spider):
     start_urls = ['http://localhost:9000/css-selector.html']
 
     def parse(self, response):
+        print response.css('.parent').extract()
+        print response.css('div:not(.child)')
+        print response.css('[class^="desc"]').extract()
+        print response.css('[class$="div"]').extract()
+        print response.css('[class~="attr"]').extract()
+        print response.css('[class|="desc"]').extract()
+        print response.css('[class*="esc"]').extract()
+        print response.css('[attr]').extract()
+        print response.css('.parent:nth-child(1)').extract()
         pass
